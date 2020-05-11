79f80953a51d7ae7df5b05a8d76bfbfa1641aa2e
cong <1836548901@qq.com>
2020-05-11 17:40:02 +0800

    可视化递归

--- DS4_Recursion/1.introduction.py
+++ DS4_Recursion/1.introduction.py
@@ -71,18 +71,96 @@ print(list_sum([1,2,3,4,5,6,7]))
         2.计算某个数的阶乘的递归算法，（最合适的基本结束条件）,假设0的阶乘为1
         5！= 5*4*3*2*1
         n == 1
+
+
+        def fact(n):
+            if n==1 or n==0:
+                return n
+            else:
+                return n*fact(n-1)
+
+
+        print(fact(5))
+
+
+
+
+四.LeetCode  第405题
+    给定一个整数，编写一个算法将这个数转化为十六进制数。
+    对于负整数，我们通常使用补码运算方法
+
+
+    给定一个整数，转化成任意进制表示的字符串格式
+    1111    转换成  字符串   1111
+    str = "0123456789"
+    769/10   = 76 余 9
+    76/10    = 7  余 6
+    7/10     = 0  余 7
+
+    srt[9] + str[6] + str[7]
+
+
+def to_str(num,base):
+    convert_str = '0123456789ABCDEF'
+    if num < base:
+        return convert_str[num]
+    else:
+        return to_str(num//base,base) + convert_str[num % base]
+
+print(to_str(769,10))
+print(to_str(769,2))
+print(to_str(769,8))
+print(to_str(769,16))
+
+
+301
+3*16^2 + 0 + 1*16^0
+
+
+1401
+1*8^3 + 4*8^2 + 0 + 1*8^0
+
+
+
+
+# 967  =>  769  栈  后进先出
+
+    -------------
+底       967        顶
+    -------------
+
+
+可以用栈的方式实现递归
+
 '''
 
+from pythonds.basic.stack import Stack
+
+s = Stack()
 
 
-def fact(n):
-    if n==1 or n==0:
-        return n
-    else:
-        return n*fact(n-1)
+def to_str(num,base):
+    convert_str = '0123456789ABCDEF'
+    while num > 0:
 
+        if num < base:
+            s.push(convert_str[num]) 
+        else:
+            s.push(convert_str[num % base])
+            
+        num = num//base
 
-print(fact(5))
+    result = ""
+    while not s.isEmpty():
+        result = result + s.pop()
+
+    return result
+
+print(to_str(796,2))
+print(to_str(796,8))
+print(to_str(796,10))
+print(to_str(796,16))
+
 
 
 
