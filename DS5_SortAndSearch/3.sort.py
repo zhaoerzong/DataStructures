0d457c11ce56cfdd4d247f64d03ad287f6110e6d
cong <1836548901@qq.com>
2020-05-14 11:49:34 +0800

    冒泡&&选择排序

--- DS5_SortAndSearch/3.sort.py
+++ DS5_SortAndSearch/3.sort.py
@@ -1,4 +1,57 @@
 '''
+    [54,26,93,17,77,31,44,55,20]
+    一、冒泡排序:对一个列表多次遍历，比较相邻的两项，并且交换顺序排错的项。
+        每对列表进行一次遍历，就有一个最大项排在了正确的位置，大体上讲，
+        列表的每一个数据项都会在其相应位置 “冒泡”
+        第一次遍历：n-1次
+        [54,26,93,17,77,31,44,55,20]    第1次    交换
+        [26,54,93,17,77,31,44,55,20]    第2次    不交换
+        [26,54,17,93,77,31,44,55,20]    第3次    交换
+        [26,54,17,77,93,31,44,55,20]    第4次    交换
+        [26,54,17,77,31,93,44,55,20]    第5次    交换
+        [26,54,17,77,31,44,93,55,20]    第6次    交换
+        [26,54,17,77,31,44,55,93,20]    第7次    交换
+        [26,54,17,77,31,44,55,20,93]    第8次    交换
+        结果：[26,54,17,77,31,44,55,20,93]
+        第二次遍历： n-2次
+
+        # 实现冒泡排序
+        # def bubbleSort(alist):
+        #     for passnum in range(len(alist)-1,0,-1):  
+        #         for i in range(passnum):   
+        #             if alist[i] > alist[i+1]:
+        #                 temp = alist[i]
+        #                 alist[i] = alist[i+1]
+        #                 alist[i+1] = temp
+        #                 # alist[i],alist[i+1] = alist[i+1],alist[i]
+        # alist = [54,26,93,17,77,31,44,55,20]
+        # bubbleSort(alist)
+        # print(alist)
+        # 假设输入的是一个已经排好序的列表  [17, 20, 26, 31, 44, 54, 55, 77, 93]
+        # 如果列表整个排序过程没有交换，说明列表已经完成了排序，因此可以通过判断有没有发生交换，改良冒泡排序
+        # 改良完了之后，就是：短路冒泡排序
+        # 实现短路冒泡排序
+        def shortBubbleSort(alist):
+            exchange = True
+            passnum = len(alist) - 1
+            while passnum > 0 and exchange:
+                exchange = False
+                for i in range(passnum):   
+                    if alist[i] > alist[i+1]:
+                        exchange = True
+                        temp = alist[i]
+                        alist[i] = alist[i+1]
+                        alist[i+1] = temp
+                passnum = passnum -1
+                        
+        alist = [54,26,93,17,77,31,44,55,20]
+        shortBubbleSort(alist)
+        print(alist)
+
+
+
+        
+        # 练习：[19,1,9,7,3,10,13,15,8,12]  写出第三次遍历之后的列表
 
 
 
